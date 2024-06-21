777c1da27f307a6e8f7c34bb51c4a421c6aba15a
HARSH DUBEY <amanad08052@gmail.com>
2024-06-21 17:16:00 +0530

    updated the sitemap

--- src/Components/MiddleInfo.jsx
+++ src/Components/MiddleInfo.jsx
@@ -15,8 +15,7 @@ function MiddleInfo(){
 
          <div>
            <h3>Gurugram: A Prime Location for Property Investment</h3>
-            <p>Known for its rapid urbanization and robust infrastructure, Gurgaon continues to attract investors seeking high returns. From new launch properties in upcoming sectors to ready-to-move options in established communities, Gurgaon offers a spectrum of opportunities.</p>
-            <p>Whether you need a high-budget or a low-budget property in Gurgaon, it is always advisable that you go through a real estate platform. As the leading experts in the local real estate market, Realty Shopee guides you through the best ready to move property in Gurgaon, ensuring you capitalise on the city's growth potential. Realty Shopee also offers resale real estate if you are looking to buy resale property in Gurgaon.</p>        </div>
+            <p>Known for its rapid urbanization and robust infrastructure, Gurgaon continues to attract investors seeking high returns. From new launch properties in upcoming sectors to ready-to-move options in established communities, Gurgaon offers a spectrum of opportunities.Whether you need a high-budget or a low-budget property in Gurgaon, it is always advisable that you go through a real estate platform. As the leading experts in the local real estate market, Realty Shopee guides you through the best ready to move property in Gurgaon, ensuring you capitalise on the city's growth potential. Realty Shopee also offers resale real estate if you are looking to buy resale property in Gurgaon.</p>        </div>
           </div>
         </div>
   )
