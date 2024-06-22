d9000f1a71ac7dfd8a8c934f835d1ee3e2efc383
HARSH DUBEY <amanad08052@gmail.com>
2024-06-22 17:25:43 +0530

    updated ui for home v2

--- src/Components/DescriptionThird.jsx
+++ src/Components/DescriptionThird.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 function DescriptionThird() {
   return (
     <div className='desc-third'>
-        <img src="https://res.cloudinary.com/dgplzytrq/image/upload/v1719049168/Description%20Images/Sachin-Gupta.jpg" alt="Sachin Sir Founder" />
+        <div className='desc3-img'>
+            <img src="https://res.cloudinary.com/dgplzytrq/image/upload/v1719049168/Description%20Images/Sachin-Gupta.jpg" alt="Sachin Sir Founder" />
+            <span className='owner'>Mr.Sachin Kumar Gupta</span> 
+        </div>
         <div className='content-desc3'>
             <h3>Connect with Realty Shopee Today</h3>
             <p>Begin the best property investment in Gurgaon with Realty Shopee today. Contact us to schedule a consultation with our experts and discover why we are the preferred choice for property seekers and investors alike. Let us help you find the best property in New Gurgaon that meets your needs and exceeds your expectations.</p>
