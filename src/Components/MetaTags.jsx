ffc4aefb974711d5d6702e40d0b372fbf1c4e0fa
HARSH DUBEY <amanad08052@gmail.com>
2024-07-01 15:14:25 +0530

    testing twitter tag

---
+++ src/Components/MetaTags.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+const MetaTags = ({ title, description, url, image }) => (
+  <Helmet>
+    <title>{title}</title>
+    <meta name="description" content={description} />
+    
+    {/* Open Graph tags */}
+    <meta property="og:title" content={title} />
+    <meta property="og:description" content={description} />
+    <meta property="og:url" content={url} />
+    <meta property="og:image" content={image} />
+    <meta property="og:type" content="website" />
+
+    {/* Twitter Card tags */}
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:title" content={title} />
+    <meta name="twitter:description" content={description} />
+    <meta name="twitter:url" content={url} />
+    <meta name="twitter:image" content={image} />
+  </Helmet>
+);
+
+export default MetaTags;
