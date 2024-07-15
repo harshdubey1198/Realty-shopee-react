c7e4de34bb6bfcf451d9c3690d0d53b335ccd68a
HARSH DUBEY <amanad08052@gmail.com>
2024-07-15 16:15:00 +0530

    canonical added

--- src/Pages/BlogDetail.jsx
+++ src/Pages/BlogDetail.jsx
@@ -3,6 +3,7 @@ import { useNavigate, useParams, Link } from 'react-router-dom';
 import axios from 'axios';
 import { FcDocument, FcHome, FcPhone, FcQuestions } from 'react-icons/fc';
 import Loader from 'react-loaders';
+import { Helmet } from 'react-helmet';
 
 function BlogDetail() {
   const navigate = useNavigate();
@@ -39,6 +40,56 @@ function BlogDetail() {
       document.title = blog.meta_title || blog.title;
     }
   }, [blog]);
+
+  const parseText = (text) => {
+    // Parse for <b> tags
+    text = text.replace(/<b>(.*?)<\/b>/g, '<strong>$1</strong>');
+
+    // Parse for <a> tags
+    text = text.replace(/<a href="(.*?)"(.*?)>(.*?)<\/a>/g, '<a href="$1"$2 style="color: darkblue; font-weight: bold;" target="_blank" rel="noopener noreferrer">$3</a>');
+
+    return text;
+  };
+
+  const renderContent = () => {
+    return JSON.parse(blog.description).map((item, index) => {
+      switch (item.type) {
+        case 'paragraph':
+          return <p key={index} dangerouslySetInnerHTML={{ __html: parseText(item.content) }} />;
+        case 'h2':
+          return <h2 key={index}>{item.content}</h2>;
+        case 'h3':
+          return <h3 key={index}>{item.content}</h3>;
+        case 'h4':
+          return <h4 key={index}>{item.content}</h4>;
+        case 'link':
+          return <a key={index} href={item.content} target="_blank" rel="noopener noreferrer">{parseText(item.content)}</a>;
+        case 'list':
+          return (
+            <ul key={index}>
+              {item.items.map((li, liIndex) => (
+                <li key={liIndex}>{parseText(li)}</li>
+              ))}
+            </ul>
+          );
+        default:
+          return null;
+      }
+    });
+  };
+
+  const renderCategoryLink = (category) => {
+    switch (category) {
+      case 'residential':
+        return <Link to="/residential-property-in-gurgaon" style={{ color: 'blue', textDecoration: 'underline' }}>Residential Properties</Link>;
+      case 'commercial':
+        return <Link to="/commercial-property-in-gurgaon" style={{ color: 'blue', textDecoration: 'underline' }}>Commercial Properties</Link>;
+      case 'sco':
+        return <Link to="/SCO-Plots-in-gurgaon" style={{ color: 'blue', textDecoration: 'underline' }}>SCO Properties</Link>;
+      default:
+        return category;
+    }
+  };
 
   if (loading) {
     return (
@@ -96,58 +147,14 @@ function BlogDetail() {
     );
   }
 
-  const parseText = (text) => {
-    // Parse for <b> tags
-    text = text.replace(/<b>(.*?)<\/b>/g, '<strong>$1</strong>');
-
-    // Parse for <a> tags
-    text = text.replace(/<a href="(.*?)"(.*?)>(.*?)<\/a>/g, '<a href="$1"$2 style="color: darkblue; font-weight: bold;" target="_blank" rel="noopener noreferrer">$3</a>');
-
-    return text;
-  };
-
-  const renderContent = () => {
-    return JSON.parse(blog.description).map((item, index) => {
-      switch (item.type) {
-        case 'paragraph':
-          return <p key={index} dangerouslySetInnerHTML={{ __html: parseText(item.content) }} />;
-        case 'h2':
-          return <h2 key={index}>{item.content}</h2>;
-        case 'h3':
-          return <h3 key={index}>{item.content}</h3>;
-        case 'h4':
-          return <h4 key={index}>{item.content}</h4>;
-        case 'link':
-          return <a key={index} href={item.content} target="_blank" rel="noopener noreferrer">{parseText(item.content)}</a>;
-        case 'list':
-          return (
-            <ul key={index}>
-              {item.items.map((li, liIndex) => (
-                <li key={liIndex}>{parseText(li)}</li>
-              ))}
-            </ul>
-          );
-        default:
-          return null;
-      }
-    });
-  };
-
-  const renderCategoryLink = (category) => {
-    switch (category) {
-      case 'residential':
-        return <Link to="/residential-property-in-gurgaon" style={{ color: 'blue', textDecoration: 'underline' }}>Residential Properties</Link>;
-      case 'commercial':
-        return <Link to="/commercial-property-in-gurgaon" style={{ color: 'blue', textDecoration: 'underline' }}>Commercial Properties</Link>;
-      case 'sco':
-        return <Link to="/SCO-Plots-in-gurgaon" style={{ color: 'blue', textDecoration: 'underline' }}>SCO Properties</Link>;
-      default:
-        return category;
-    }
-  };
-
   return (
     <div className="blog-detail">
+      <Helmet>
+        <title>{blog.meta_title || blog.title}</title>
+        <link rel="canonical" href={`https://www.realtyshopee.com/blogs/${meta_url}`} />
+        <meta name="description" content={blog.meta_description || blog.description} />
+        <meta name="keywords" content={(blog.meta_keywords || []).join(', ')} />
+      </Helmet>
       <header className='pd-nav'>
         <h2><Link to="https://www.realtyshopee.com" className='r-logo'><img src="https://www.realtyshopee.com/static/media/Realty%20shopee%20main%20logo.db6affde1f766278cf09.png" className='pd-logo' alt='Realty-Shopee-blogs' /></Link></h2>
         <a href="/" target="_blank"><span>Home</span><span><FcHome /></span></a>
