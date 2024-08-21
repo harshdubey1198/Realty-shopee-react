50182ddabf1cd5fdd2788763c687f9e153defa79
HARSH DUBEY <amanad08052@gmail.com>
2024-08-21 18:22:55 +0530

    ui changes for whyshopee v2

--- src/Components/MiddleInfo.jsx
+++ src/Components/MiddleInfo.jsx
@@ -13,7 +13,7 @@ function MiddleInfo(){
             <h2>Why Choose Realty Shopee?</h2>
             <p>As the best property management company in Gurgaon <b>Realty Shopee</b> stands out for its commitment to client satisfaction and comprehensive real estate solutions. Our team of experts leverages extensive market knowledge and industry connections to help you make informed decisions on current and new upcoming properties in Gurgaon. Whether you're a first-time buyer, a seasoned investor, or looking for property management services, we tailor our approach to meet your unique requirements.</p>
           </div>
-          <img src='https://res.cloudinary.com/dgplzytrq/image/upload/v1719038705/Description%20Images/Why-realty-shopee-blue.png' loading='lazy' alt='Why Realty Shopee'/>
+          <img src='https://res.cloudinary.com/dgplzytrq/image/upload/v1719038705/Description%20Images/Why-realty-shopee-blue.png' loading='lazy' className='whyshopee' alt='Why Realty Shopee'/>
          </div>
 
          <div>
